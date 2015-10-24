53b12b800559ad34f60c2ab331c75b027a51ab16
kylehl <kylehl1@uci.edu>
2015-10-23 20:32:47 -0700

    Testing publish/subscribe
    First project, very bare skeleton/pseudocode, still figuring out syntax

---
+++ test_due_pubsub/test_due_pubsub/test_due_pubsub.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		test_due_pubsub.ino
+ Created:	10/23/2015 7:41:59 PM
+ Author:	Kyle
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
