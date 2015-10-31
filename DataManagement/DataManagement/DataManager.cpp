8abcde4f48a6192f9f113f4cd478c6dcae36cc35
kylehl <kylehl1@uci.edu>
2015-10-31 13:17:53 -0700

    Alpha implementation of pub/sub, deleted junk files
    Got rid of unnecessary files, publish/subscribe functions are
    tentatively implemented

--- DataManagement/DataManagement/DataManager.cpp
+++ DataManagement/DataManagement/DataManager.cpp
@@ -2,8 +2,9 @@
 // File: DataManager.cpp
 // Contributors: Iniyavan Sathiamurthi, Jason Watkins, Kelly Ho, Kyle Lam
 // Created: 10/27/2015
-// Last Modified: 10/27/2015
+// Last Modified: 10/31/2015
 
+#include <iostream>
 #include <map>
 #include <vector>
 
@@ -17,18 +18,43 @@ namespace DataManagement
 	// publish called by components with new data
 	static void Publish(const std::string& type, void* data, std::size_t len)
 	{
-		std::pair < const std::string&, std::vector<void(void*, std::size_t)> > tempPair;
-		std::vector < void(void*, std::size_t) > tempVect;
-		tempVect.push_back(void(*, len));
-		tempPair = std::make_pair(type, );
-		//std::vector<void<void*, std::size_t>>
-		subscribers.insert((type, ));
-		// TODO needs lots of work
+		// key not found, send error message to publisher
+		if (subscribers.count(type) == 0)
+		{
+			std::cerr << "Please run Subscriber function first before Publish";
+		}
+
+		// iterate through associated vector
+		else
+		{
+			std::vector< void(void*, std::size_t) > value;
+			value = subscribers[type];
+
+			for (int i = 0; i < value.size(); i++)
+			{
+				value[i](data, len);
+			}
+		}
 	}
 
 	// subscribe called by components who want certain data
-	static void Subscribe(const std::string& type, void(void*, std::size_t))
+	static void Subscribe(const std::string& type, void(ptr)(void*, std::size_t))
 	{
+		// if key does not exist, add to map with empty vector
+		if (subscribers.count(type) == 0)
+		{
+			std::pair<const std::string&, std::vector<void(void*, std::size_t)>> tempPair;
+			std::vector<void(void*, std::size_t)> emptyVect;
+			tempPair = std::make_pair(type, emptyVect);
+			subscribers.insert(tempPair);
+		}
 
+		// if the key does exist, add the pointer into the vector
+		else
+		{
+			std::vector<void(void*, std::size_t)> tempVect;
+			tempVect = subscribers[type];
+			tempVect.push_back(*ptr);
+		}
 	}
 }
