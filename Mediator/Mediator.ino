1aa5a1dd39f34533b956427ff5e1c51882054e32
Jason Watkins <jason-watkins@outlook.com>
2015-11-16 10:09:24 -0800

    Fixed typo.

--- Mediator/Mediator.ino
+++ Mediator/Mediator.ino
@@ -1,5 +1,5 @@
 // Copyright (c) 2015:
-// Jason Watkins <watkins1@udi.edu>
+// Jason Watkins <watkins1@uci.edu>
 //
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
