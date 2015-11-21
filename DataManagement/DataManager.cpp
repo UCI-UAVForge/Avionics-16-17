0a378cde2fc0086e8d4ed355a282beb065525cbc
Iniyavan Sathiamurthi <isathiam@uci.edu>
2015-11-21 13:01:49 -0800

    My rough draft for the comments.

--- DataManagement/DataManager.cpp
+++ DataManagement/DataManager.cpp
@@ -2,7 +2,7 @@
 // File: DataManager.cpp
 // Contributors: Iniyavan Sathiamurthi, Jason Watkins, Kelly Ho, Kyle Lam
 // Created: 10/27/2015
-// Last Modified: 11/14/2015
+// Last Modified: 10/31/2015
 
 #include <map>
 #include <vector>
@@ -11,45 +11,61 @@
 
 namespace DataManagement
 {
-	// map to store key/vector pairs
-	static std::map < std::string, std::vector<void(*)(void*, std::size_t)> > subscribers;
+	/*! map stores the key has a string and an array of function pointers*/
+	static std::map < const std::string&, std::vector<void(*)(void*, std::size_t)> > subscribers;
 
-	// publish called by components with new data
-	void DataManager::Publish(std::string type, void* data, std::size_t len)
+	/*Publish method, publishes data to the subscribers*/
+	static void Publish(const std::string& type, void* data, std::size_t len)
 	{
-		// key not found, send error message to publisher
+		/*!
+			If key is not found error messages displays to call the 
+			subscribe fucnction first. 
+		*/
 		if (subscribers.count(type) == 0)
 		{
 			std::cerr << "Please run Subscriber function first before Publish";
 		}
 
-		// iterate through associated vector
+		/*!
+			If key exists, then send the data to all the fucntion pointers
+			that are subscribed to that key. 
+		*/
 		else
 		{
+			
 			std::vector< void(*)(void*, std::size_t) > value;
 			value = subscribers[type];
-
+			
+			/*! 
+				Loops through all the pointers and sends the pointer
+				to the data and the size to all the fucntion pointers.
+			*/				
 			for (int i = 0; i < value.size(); i++)
 			{
-				value[i](data, len);
+				value[i](data, len); /*! Function pointer at each index of the array*/
 			}
 		}
 	}
 
-	// subscribe called by components who want certain data
-	void DataManager::Subscribe(std::string type, void(*ptr)(void*, std::size_t))
+	/*! Subscribe method adds either an new key or function pointers*/ 
+	static void Subscribe(const std::string& type, void(*ptr)(void*, std::size_t))
 	{
-		// if key does not exist, add to map with empty vector
+		/*! If key does not exist then add it to the map*/
 		if (subscribers.count(type) == 0)
 		{
-			std::pair<std::string, std::vector<void(*)(void*, std::size_t)>> tempPair;
-			std::vector<void(*)(void*, std::size_t)> emptyVect;
+			/*! Makes a pair to be stored in the vector*/
+			std::pair<const std::string&, std::vector<void(*)(void*, std::size_t)>> tempPair;
+			/*! Initializes a new vector for that particualr key*/
+			std::vector<void(*)(void*, std::size_t)>* emptyVect = new std::vector<void(*)(void*, std::size_t)>();
+			/*! Makes a pair and stores into the tempPair*/
 			tempPair = std::make_pair(type, emptyVect);
+			/*! Appends it the map*/
 			subscribers.insert(tempPair);
 		}
 
-		// add the pointer into the vector
-		std::vector<void(*)(void*, std::size_t)>& tempVect = subscribers[type];
+		
+		std::vector<void(*)(void*, std::size_t)> tempVect;
+		tempVect = subscribers[type];
 		tempVect.push_back(ptr);
 	}
 }
--- DataManagement/DataManager.h
+++ DataManagement/DataManager.h
@@ -12,14 +12,44 @@
 #include <iostream>
 #include <stdlib.h>
 
+//! DataManagement own namesapace.
+/*!
+	DataManagement has its own namespace so that the variables that 
+	are declared in this namspace are not interfered with any other 
+	variables names outside of the namespace. 
+*/
 namespace DataManagement
 {
+//! DataManager class.
+/*!
+	The DataManager class contains the publish and subscribe 
+	pattern that is implemented in this class. The whole purpose
+	of this class is for publishers like autopilot can send data to
+	the publish class and subscribers like radio comms and ground
+	station will be able to retreive that data if they are subsribed 
+	to the data. 
+*/
 	class DataManager
 	{
 	public:
-		static void Publish(std::string type, void* data, std::size_t len);
-
-		static void Subscribe(std::string type, void(void*, std::size_t));
+		/*!
+			Publish method is takes in a string type which is a key,
+			pointer to the data, and the size of that data. Using the 
+			pointer data, Publish method looks up the key in the map 
+			and passes the data to the functions pointers that are 
+			subscribed. 
+		*/
+		static void Publish(const std::string& type, void* data, std::size_t len);
+		
+		/*!
+			Subscribe method takes in the string type whcih is a key.
+			If the key does not exist in the map then Publish mehtod
+			creates the key and ignores the other paramenter. If the 
+			key already exists, then the Subscribe method appends the
+			funtion pointer(2nd parameter) to the value of the map which
+			is an array of function pointers.
+		*/
+		static void Subscribe(const std::string& type, void(void*, std::size_t));
 	};
 }
 
